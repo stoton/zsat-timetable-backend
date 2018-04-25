34988973fd1b4198c5f89c91d57944dff6cce65a
Dominik Toton <dominik.toton@protonmail.com>
2018-04-25 22:58:22 +0200

    Add default constructors to allow Jackson to work

--- src/main/java/com/github/stoton/domain/CompleteTimetable.java
+++ src/main/java/com/github/stoton/domain/CompleteTimetable.java
@@ -1,12 +1,16 @@
 package com.github.stoton.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompleteTimetable {
 
     private List<Lesson> monday;
--- src/main/java/com/github/stoton/domain/Lesson.java
+++ src/main/java/com/github/stoton/domain/Lesson.java
@@ -1,12 +1,16 @@
 package com.github.stoton.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Lesson {
 
     private String lessonNumber;
