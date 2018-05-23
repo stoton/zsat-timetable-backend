ab156dd19d4becbdb54b34c947474aa1eaa73080
Niizuma <sebastian.toton@onet.pl>
2018-05-23 18:10:15 +0200

    fixed swagger mapping

--- src/main/java/com/github/stoton/App.java
+++ src/main/java/com/github/stoton/App.java
@@ -14,24 +14,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 @EnableScheduling
 @EnableSpringConfigured
-public class App extends WebMvcConfigurerAdapter {
+public class App extends SpringBootServletInitializer {
 
    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/documentation/v2/api-docs", "/v2/api-docs").setKeepQueryParams(true);
-        registry.addRedirectViewController("/documentation/swagger-resources/configuration/ui","/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/documentation/swagger-resources/configuration/security","/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/documentation/swagger-resources", "/swagger-resources");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/documentation/**").addResourceLocations("classpath:/META-INF/resources/");
-    }
-
-
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+       return builder.sources(App.class);
+   }
 }
--- src/main/java/com/github/stoton/configuration/AppConfiguration.java
+++ src/main/java/com/github/stoton/configuration/AppConfiguration.java
@@ -3,12 +3,28 @@ package com.github.stoton.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan("com.github.stoton")
 @EnableAspectJAutoProxy
 @EnableSwagger2
-public class AppConfiguration {
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/documentation/v2/api-docs", "/v2/api-docs").setKeepQueryParams(true);
+        registry.addRedirectViewController("/documentation/swagger-resources/configuration/ui","/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/documentation/swagger-resources/configuration/security","/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/documentation/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/documentation/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
 
 }
